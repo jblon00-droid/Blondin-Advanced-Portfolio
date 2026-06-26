9a25bd603cb10f554f7ff1c26fba382bd2a31ad2
jblon00-droid <jblon00@gmail.com>
2026-06-26 15:20:47 -0500

    finished

--- app.js
+++ app.js
@@ -2,6 +2,34 @@
 // service_18vh5sr
 // user__kpWU_hI5CxB0_IuKy
 
+let isModalOpen = false
+let contrastToggle = false
+const scaleFactor = 1 / 20
+
+function moveBackground(event) {
+    const shapes = document.querySelectorAll(".shape");
+    const x = event.clientX * scaleFactor;
+    const y = event.clientY * scaleFactor;
+
+    for (let i = 0; i < shapes.length; ++i) {
+        const isOdd = i % 2 !== 0;
+        const boolInt = isOdd ? -1 : 1;
+        shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+    }
+}
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else{
+        document.body.classList.remove("dark-theme") 
+    }
+}
+
+
+
 function contact(event) {
     event.preventDefault();
     const loading = document.querySelector('.modal__overlay--loading')
@@ -24,6 +52,13 @@ function contact(event) {
      })
 }
 
+
+
 function toggleModal() {
+    if (isModalOpen){
+        isModalOpen = false
+        return document.body.classList.remove("modal--open")
+    }
+    isModalOpen = true
     document.body.classList += " modal--open"
 }
